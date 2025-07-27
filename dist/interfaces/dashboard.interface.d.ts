07453a1b57ceb2e9d542173ab02a07a3fd81f6d2
Tyler Priest <hello@tylerpriest.com>
2025-07-27 22:46:15 +1000

    Remove all Vercel-related files and configurations

--- dist/interfaces/dashboard.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { DashboardWidget, DashboardLayout } from '../types';
-export interface IDashboardManager {
-    registerWidget(widget: DashboardWidget): void;
-    unregisterWidget(widgetId: string): void;
-    getActiveWidgets(): DashboardWidget[];
-    updateLayout(layout: DashboardLayout): Promise<void>;
-    showWelcomeScreen(): void;
-}
-//# sourceMappingURL=dashboard.interface.d.ts.map
--- dist/interfaces/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './auth.interface';
-export * from './plugin.interface';
-export * from './settings.interface';
-export * from './ui.interface';
-export * from './dashboard.interface';
-export * from './admin.interface';
-export * from './logging.interface';
-export * from './testing.interface';
-//# sourceMappingURL=index.d.ts.map
